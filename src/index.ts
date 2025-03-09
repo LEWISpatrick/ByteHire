3cbf32fa357bb3fd0a3af9b190c77ce84a11f515
patrick hp <patricklewis2009@gmail.com>
2025-03-09 11:10:50 -0300

    (feat) added landdiing page and login

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
 
+app.get('/signup', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/signup.html"))
+});
+
 console.log('app ran')
 
 
 app.listen(port,() => {
     console.log(`Server is running at http://localhost:${port}`);
 })
+
+
+
+
